4a2e17087ecf55d296f3f3047d2d310e174781d1
Sudhakar Sharma <117532270+sudhakarsharma07@users.noreply.github.com>
2025-06-30 13:49:24 +0530

    Update MainLayout.jsx

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -9,8 +9,10 @@ const MainLayout = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Navbar />
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4 md:p-6">
-          <Outlet /> {/* Page content will be rendered here */}
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-4 md:p-6">
+          <div className="bg-white shadow-sm rounded-xl p-4 md:p-6 min-h-[calc(100vh-4rem)] animate-fade-in">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
